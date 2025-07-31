de190cd471515d407b34e12aa93f77134e7263f6
aleempasha2772 <aleempasha1410@gmail.com>
2025-07-31 18:41:25 +0530

    PolishNotation : Stack

---
+++ src/main/java/com/example/neetcode_150/SlidingWindow/PolishNotation.java
@@ -0,0 +1,41 @@
+package com.example.neetcode_150.SlidingWindow;
+
+import java.util.Stack;
+
+public class PolishNotation {
+	
+	public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String c : tokens){
+            if(c.equals("+")) {
+            	stack.push(stack.pop() + stack.pop());
+            }else if(c.equals("-")) {
+            	int a = stack.pop();
+            	int b = stack.pop();
+            	stack.push(b-a);
+            }else if(c.equals("*")) {
+            	stack.push(stack.pop() * stack.pop());
+            }else if(c.equals("/")) {
+            	int a = stack.pop();
+            	int b = stack.pop();
+            	if(a == 0|| b==0) {
+            		throw new ArithmeticException("division is not possible");
+            	}
+            	stack.push(b/a);
+            }else {
+            	stack.push(Integer.parseInt(c));
+            }
+        }
+        
+        return stack.pop();
+    }
+	
+	
+	public static void main(String[] args) {
+		String[] tokens = {"1","2","+","3","*","4","-"};
+		System.out.println(evalRPN(tokens));
+	}
+	
+	
+}
